eb605af78b4293253f14f1598e47bdc62bba4db1
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-11-18 19:20:22 +0530

    added ratio initial

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def index():
     with open ("data.json", "r") as f:
         companies = json.load(f).keys()
-    print(companies)
     return render_template("index.html", companies = companies)
 
 @app.route("/company/<name>/", methods=["GET"])
@@ -16,7 +15,30 @@ def company_data(name):
         data = json.load(f)[name]
     return render_template("company.html", data = data)
 
+@app.route("/pe-ratio/", methods=["GET", "POST"])
+def pe_ratio():
+    if request.method == "GET":
+        return render_template("pe-ratio.html", companies = {})
 
+    elif request.method == "POST":
+        start = float(request.form['start'])
+        end = float(request.form['end'])
+
+        with open("data.json", "r") as f:
+            data = json.load(f)
+        
+        companies = dict()
+
+        for key, value in data.items():
+            try:
+                if (value["P/E Ratio TTM"][0] == '-' or float(value["P/E Ratio TTM"][0].replace(",", "")) >= start) and \
+                    (value["P/E Ratio TTM"][1] == '-' or float(value["P/E Ratio TTM"][1].replace(",", "")) <= end):
+                    companies[key] = value["P/E Ratio TTM"]
+            except Exception as e:
+                print(e)
+                pass
+
+        return render_template("pe-ratio.html", companies = companies)
 
 if __name__ == "__main__":
     # create_db()
